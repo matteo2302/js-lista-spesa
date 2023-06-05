a965ba8060af79110898f24c94e998b3a9ae16e6
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-05 14:50:31 +0200

    done?

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("JSOK");
+/*********************fase 0***********************/
+//prendo l'ul
+const ul = document.querySelector("ul");
+//array del cibo
+const ingredients = ["patate", "cipolle", "uova", "burro", "sale", "pepe"];
+
+let i = 0;
+while (i !== ingredients.length) {
+  ul.innerHTML += `<li> ${ingredients[i]}</li>`;
+  i++;
+}
--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@
   - creiamo un ul nell'html
   - colleghiamo l'ul a js
   - creiamo un array con nomi di cibo all'interno
+  - creiamo una variabile i per fare il conto
 - fase 1
   - creiamo un ciclo che continui finche il valore della i sarà uguale a quello dell'array dell'array
   - mettiamo nell'inner html gli li con i valori dell'array
